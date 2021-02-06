966d7ddc8d9f8147d21b1fa33f4d754a74ee14d8
Craig Freeburg <craigfreeburg@gmail.com>
2021-02-06 12:50:30 -0600

    project done

--- index.php
+++ index.php
@@ -1,87 +1,52 @@
 <?php
-    //Set first name 
-    $fName = $_GET['first'];
+require('database.php');
 
-    //Set last name
-    $lName = $_GET['last'];
 
-    //Create full name
-    $fullName = $fName.' '.$lName;
-    
-    //Set age
-    $age = $_GET['age'];
+$query = '';// PUT YOUR SQL QUERY HERE
+// Example: $query = 'SELECT * FROM customers';
+$query = 'SELECT C.customerID, C.emailAddress, C.firstName, C.lastName, A.line1, A.city, A.state, A.zipCode, A.phone FROM customers C
+INNER JOIN addresses A
+ON C.customerID = A.customerID
+GROUP BY C.customerID, C.emailAddress, C.firstName, C.lastName, A.line1, A.city, A.state, A.zipCode, A.phone';
 
-    //Confirm age
-    $goodAge = filter_var($age, FILTER_VALIDATE_INT); 
-    
-    //Set date
-    $date = date('m/d/Y');
 
-    //Find age in days IF good age submitted
-    if ($goodAge){
-        $ageDays = $age * 365;
-    }
+$statement = $db->prepare($query);
+$statement->execute();
+$customers = $statement->fetchAll();
+$statement->closeCursor(); 
 
-    //Set if old enough to vote
-    if ($ageDays >= 6570) {
-        $voter = true;
-    }
-    else {
-        $voter = false;
-    }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>Craig Freeburg INF 653 Week 2 GET Project</title>
+    <title>My Guitar Shop Customers</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
+
+<!-- the body section -->
 <body>
-    <header id="dateHeader">
-        <p id="date"> Today is <?php echo htmlspecialchars($date) ?> </p>
-    </header>
+    <header><h1>Customer List</h1></header>
     <main>
-        <br><br><br><br><br><br><br><br><br><br><br><br>
-        <h1 id="name">
-            <?php 
-            if (empty($lName) || empty($fName)){
-                echo "You did not submit name parameters.";
-                exit();
-            }
-            elseif (empty($age) || $goodAge == false){
-                echo "You did not submit a numeric age parameter.";
-                exit();
-            }
-            else {
-                echo "Hello, my name is " . htmlspecialchars($fullName) . ".";
-            }
-            ?>
-        </h1>
-        <p id="voter">
-            <?php
-                if ($voter == true){
-                    echo "I am " . htmlspecialchars($age) . " years old, and I am old enough to vote in the United States.";
-                }
-                else {
-                    echo "I'm only " . htmlspecialchars($age) . ", which means I'm not old enough to vote.";
-                };
-            ?>
-        </p>
-        <p id="daysAge">
-            <?php
-                if ($voter == true){
-                    echo "That means I'm at least " . htmlspecialchars(number_format($ageDays)).  " days old.";
-                }
-                else {
-                    echo "I'm " . htmlspecialchars(number_format($ageDays)).  " days old, and I need to be 6,570 days old to vote in the United States.";
-                };
-            ?>
-        </p>
+        <section>
+            <?php foreach ($customers as $customer) : ?>
+                <p><span class="bold">CustID:</span> <?php echo $customer['customerID']; ?></p>
+                <p><span class="bold">Email:</span> <?php echo $customer['emailAddress']; ?></p>
+                <p><span class="bold">FirstName:</span> <?php echo $customer['firstName']; ?></p>
+                <p><span class="bold">LastName:</span> <?php echo $customer['lastName']; ?></p>
+                <p><span class="bold">Address:</span> <?php echo $customer['line1']; ?></p>
+                <p><span class="bold">City:</span> <?php echo $customer['city']; ?></p>
+                <p><span class="bold">State:</span> <?php echo $customer['state']; ?></p>
+                <p><span class="bold">ZipCode:</span> <?php echo $customer['zipCode']; ?></p>
+                <p><span class="bold">Phone:</span> <?php echo $customer['phone']; ?></p>
+                <br>
+            <?php endforeach; ?>
+        </section>
     </main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+    </footer>
 </body>
-
 </html>
-
-
